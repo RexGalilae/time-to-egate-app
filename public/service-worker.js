221a3e3bccbda4ed289cce8b964ad20ab97a46fe
Zaid Hassan <mz592@cornell.edu>
2024-09-19 21:39:25 +0400

    feat: Cache all assets so the app works offline

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,6 +4,20 @@ const urlsToCache = [
 	'/index.html',
 	'/time-to-egate-app/assets/icons/icon-192x192.png',
 	'/time-to-egate-app/assets/icons/icon-512x512.png',
+	'/time-to-egate-app/assets/index-DN6AwBez.js',
+	'/time-to-egate-app/assets/clock-yoda.json',
+	'/time-to-egate-app/icon.svg',
+	'/time-to-egate-app/assets/thinking-yoda.json',
+	'/time-to-egate-app/assets/thinking-utya.json',
+	'/time-to-egate-app/assets/loading-bg.svg',
+	'/time-to-egate-app/assets/here-you-go.webp',
+	'/time-to-egate-app/assets/schedule.csv',
+	'/time-to-egate-app/assets/pleepind-utya.json',
+	'/time-to-egate-app/assets/pleepind-yoda.json',
+	'/time-to-egate-app/assets/nahaind-utya.json',
+	'/time-to-egate-app/assets/flying-utya.json',
+	'/time-to-egate-app/assets/wide-minna-of-doom.png',
+	'/time-to-egate-app/assets/oh-my-god-bruh-oh-hell-nah.mp3',
 	// Add other assets you want to cache
 ];
 
